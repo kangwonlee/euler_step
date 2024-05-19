a1a1fc7e24c94bb948b93c2e9dee12008818d2f8
KangWon LEE <kangwon@gmail.com>
2024-05-19 16:09:19 +0900

    check type

--- tests/test_results.py
+++ tests/test_results.py
@@ -72,6 +72,16 @@ def t_sec(t_discharge_sec:float) -> float:
 
 def test_dh_dt(t_sec:float, h_m:float, a_m2:float, q_m3ps:float, A_m2:float, g_mpsps:float):
     result = mch.dh_dt(t_sec, h_m, a_m2, A_m2, g_mpsps)
+
+    assert isinstance(result, float), (
+        f"input arguments: t={t_sec}, h={h_m}, a={a_m2}, A={A_m2}, g={g_mpsps}\n"
+        f"입력 매개변수: t={t_sec}, h={h_m}, a={a_m2}, A={A_m2}, g={g_mpsps}\n"
+        f"Expected: {q_m3ps / A_m2}\n"
+        f"예상값: {q_m3ps / A_m2}\n"
+        f"Got: {result}\n"
+        f"받은 값: {result}\n"
+    )
+
     assert result == pytest.approx(q_m3ps / A_m2), (
         f"input arguments: t={t_sec}, h={h_m}, a={a_m2}, A={A_m2}, g={g_mpsps}\n"
         f"입력 매개변수: t={t_sec}, h={h_m}, a={a_m2}, A={A_m2}, g={g_mpsps}\n"
